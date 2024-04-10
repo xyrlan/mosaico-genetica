9bf2cb0f3d22b0c3be1f179172e10a861d9851be
xyrlan <lobatopedro68@gmail.com>
2024-04-10 17:28:52 -0400

    Integrate @splinetool/react-spline into SplineViewer component and update layout in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,41 @@
 'use client'
-import React from "react";
+import React, { Suspense } from "react";
 import Image from "next/image";
 import { motion } from "framer-motion"
 import SplineViewer from "./components/SplineViewer";
+import Script from "next/script";
+
+
+
 
 export default function Home() {
   return (
     <>
-      <SplineViewer />
-      <>
-        <main role="main" className="flex items-center min-h-screen p-4 md:p-24 relative overflow-hidden ">
-          <div className="z-30">
-            <motion.div
-              initial={{ opacity: 0, x: -100 }}
-              whileInView={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.5 }}
-            >
-              <Image src="/slogan-mosaico.png" alt="Mosaico Genética" width={800} height={800} priority className="w-auto h-auto" />
-            </motion.div>
-            <motion.p
-              initial={{ opacity: 0, y: 100 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5 }}
-              className="text-lg md:text-xl font-medium text-gray-600 max-w-2xl text-center text-balance">
-              Um consultório médico dedicado ao diagnóstico, acompanhamento e aconselhamento genético de famílias com doenças raras.
-              <br />
-              Atendimento de genética médica presencial em Brasília (DF) e online em todo o Brasil.
-            </motion.p>
-          </div>
-          <div className="hidden max-md:block absolute top-0 h-full w-full bg-white/20 z-20" />
-        </main>
-        <div className="min-h-screen bg-black flex items-center"> blablabla</div>
-      </>
+      <main role="main" className="flex items-center min-h-screen p-4 md:p-24 relative overflow-hidden ">
+        <div className="z-30">
+          <motion.div
+            initial={{ opacity: 0, x: -100 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            <Image src="/slogan-mosaico.png" alt="Mosaico Genética" width={800} height={800} priority className="w-auto h-auto" />
+          </motion.div>
+          <motion.p
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            className="text-lg md:text-xl font-medium text-gray-600 max-w-2xl text-center text-balance">
+            Um consultório médico dedicado ao diagnóstico, acompanhamento e aconselhamento genético de famílias com doenças raras.
+            <br />
+            Atendimento de genética médica presencial em Brasília (DF) e online em todo o Brasil.
+          </motion.p>
+        </div>
+
+        <SplineViewer />
+
+        <div className="hidden max-md:block absolute top-0 h-full w-full bg-white/20 z-20" />
+      </main>
+      <div className="min-h-screen bg-black flex items-center"> blablabla</div>
 
     </>
   );
