1c2215135c365177905180134d4ba3fe960c959a
xyrlan <lobatopedro68@gmail.com>
2024-04-10 14:20:08 -0400

    Refactor page.tsx to remove unused imports and add new layout and animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 'use client'
-import Head from "next/head";
 import Image from "next/image";
-import Script from "next/script";
 import { motion } from "framer-motion"
 
 export default function Home() {
