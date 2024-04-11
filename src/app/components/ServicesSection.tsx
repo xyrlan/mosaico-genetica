a69f784668301d6794f910f77f6b276370e618e2
xyrlan <lobatopedro68@gmail.com>
2024-04-11 10:47:23 -0400

    Update ServicesSection component with new icons and descriptions

--- src/app/components/ServicesSection.tsx
+++ src/app/components/ServicesSection.tsx
@@ -6,32 +6,32 @@ const ServicesSection = () => {
     {
       name: 'Erros Inatos do Metabolismo',
       description: 'Nossa equipe especializada em Erros Inatos do Metabolismo está aqui para desvendar os complexos desafios metabólicos.',
-      icon: '/path/to/erros-inatos-do-metabolismo-icon.png'
+      icon: '/metabolism.png'
     },
     {
       name: 'Neurogenética',
       description: 'Na Mosaico, compreendemos as nuances da Neurogenética, focando em condições neurológicas hereditárias e oferecendo soluções clínicas avançadas para enfrentar desafios neurogenéticos.',
-      icon: '/path/to/neurogenetica-icon.png'
+      icon: '/neurogenetica.png'
     },
     {
       name: 'Genética Reprodutiva',
       description: 'Em Genética Reprodutiva, concentramo-nos em auxiliar casais que buscam orientação sobre riscos genéticos durante a gestação.',
-      icon: '/path/to/genetica-reprodutiva-icon.png'
+      icon: '/fertilization.png'
     },
     {
       name: 'Oncogenética',
       description: 'Para aqueles enfrentando desafios oncológicos hereditários, nossa equipe proporciona aconselhamento e estratégias personalizadas de monitoramento.',
-      icon: '/path/to/oncogenetica-icon.png'
+      icon: '/dna.png'
     },
     {
       name: 'Interpretação de Exames Genéticos',
       description: 'Na era da genômica, oferecemos interpretação avançada de exames genéticos.',
-      icon: '/path/to/interpretacao-exames-geneticos-icon.png'
+      icon: '/search.png'
     },
     {
       name: 'Pareceres',
       description: 'Nossa equipe atua em estreita colaboração com hospitais, proporcionando pareceres genéticos abrangentes para crianças enfrentando desafios médicos complexos.',
-      icon: '/path/to/pareceres-icon.png'
+      icon: '/clipboard.png'
     }
   ];
 
@@ -44,9 +44,9 @@ const ServicesSection = () => {
         </div>
         <dl className="mt-14 text-base leading-7 text-gray-600 lg:max-w-none grid md:grid-cols-2 gap-8">
           {services.map((service) => (
-            <div key={service.name} className="relative pl-9 flex flex-col items-center bg-gray-200 rounded-2xl p-8 hover:-translate-y-2 duration-300 transition-all hover:text-white shadow-lg hover:shadow-2xl">
-              <Image src={service.icon} alt={service.name} width={600} height={600} className='h-[150px] md:h-[300px] w-auto object-center' />
-              <dt className="font-semibold  justify-center text-3xl mt-5">
+            <div key={service.name} className="relative pl-9 flex flex-col items-center bg-gray-200 rounded-2xl p-8 hover:-translate-y-2 duration-300 transition-all shadow-lg hover:shadow-2xl">
+              <Image src={service.icon} alt={service.name} width={600} height={600} className='h-[150px] md:h-[300px] w-auto object-center opacity-70' />
+              <dt className="font-semibold  justify-center text-3xl mt-5 text-center">
                 {service.name}
               </dt>
               <dd className="mt-3">{service.description}</dd>
