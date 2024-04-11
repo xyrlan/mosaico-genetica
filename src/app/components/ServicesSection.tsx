c5099cd3d1fb40ae35331bee8ded772ec38be82e
xyrlan <lobatopedro68@gmail.com>
2024-04-11 10:00:01 -0400

    Integrate lucide-react into components and update dependencies in package.json

---
+++ src/app/components/ServicesSection.tsx
@@ -0,0 +1,62 @@
+import Image from 'next/image'
+import React from 'react'
+
+const ServicesSection = () => {
+  const services = [
+    {
+      name: 'Erros Inatos do Metabolismo',
+      description: 'Nossa equipe especializada em Erros Inatos do Metabolismo está aqui para desvendar os complexos desafios metabólicos.',
+      icon: '/path/to/erros-inatos-do-metabolismo-icon.png'
+    },
+    {
+      name: 'Neurogenética',
+      description: 'Na Mosaico, compreendemos as nuances da Neurogenética, focando em condições neurológicas hereditárias e oferecendo soluções clínicas avançadas para enfrentar desafios neurogenéticos.',
+      icon: '/path/to/neurogenetica-icon.png'
+    },
+    {
+      name: 'Genética Reprodutiva',
+      description: 'Em Genética Reprodutiva, concentramo-nos em auxiliar casais que buscam orientação sobre riscos genéticos durante a gestação.',
+      icon: '/path/to/genetica-reprodutiva-icon.png'
+    },
+    {
+      name: 'Oncogenética',
+      description: 'Para aqueles enfrentando desafios oncológicos hereditários, nossa equipe proporciona aconselhamento e estratégias personalizadas de monitoramento.',
+      icon: '/path/to/oncogenetica-icon.png'
+    },
+    {
+      name: 'Interpretação de Exames Genéticos',
+      description: 'Na era da genômica, oferecemos interpretação avançada de exames genéticos.',
+      icon: '/path/to/interpretacao-exames-geneticos-icon.png'
+    },
+    {
+      name: 'Pareceres',
+      description: 'Nossa equipe atua em estreita colaboração com hospitais, proporcionando pareceres genéticos abrangentes para crianças enfrentando desafios médicos complexos.',
+      icon: '/path/to/pareceres-icon.png'
+    }
+  ];
+
+  return (
+    <section id='servicos' className='min-h-screen relative py-24  flex flex-col items-center'>
+      <div className='container'>
+        <div className='flex flex-col items-center gap-7'>
+          <h2 className='text-4xl font-semibold text-center max-w-2xl'>Serviços que estão além do diagnóstico</h2>
+          <p className='text-lg md:text-xl font-medium text-gray-600 max-w-4xl text-center text-balance tracking-wide'>Entendemos que cada jornada na genética é única, e na Mosaico abraçamos oferecemos aconselhamento nas diversas áreas da genética clínica para oferecer suporte holístico às necessidades específicas de cada paciente.</p>
+        </div>
+        <dl className="mt-10 text-base leading-7 text-gray-600 lg:max-w-none grid md:grid-cols-2 gap-8">
+          {services.map((service) => (
+            <div key={service.name} className="relative pl-9 flex flex-col items-center bg-gray-200 rounded-2xl p-8 hover:-translate-y-2 duration-300 transition-all hover:text-white">
+              <Image src={service.icon} alt={service.name} width={600} height={600} className='h-[150px] md:h-[300px] w-auto object-center' />
+              <dt className="font-semibold  justify-center text-3xl mt-5">
+                {service.name}
+              </dt>
+              <dd className="mt-3">{service.description}</dd>
+            </div>
+          ))}
+        </dl>
+
+      </div>
+    </section>
+  )
+}
+
+export default ServicesSection
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 'use client'
 import React from "react";
+// Sections
 import HeroSection from "./components/HeroSection";
-import Image from "next/image";
 import SplineViewer from "./components/SplineViewer";
+import AboutSection from "./components/AboutSection";
+import ServicesSection from "./components/ServicesSection";
 
 
 export default function Home() {
@@ -12,20 +14,8 @@ export default function Home() {
         <HeroSection />
         <SplineViewer />
       </main>
-      <section id="nosso-especialista" className=" min-h-screen flex items-center z-50">
-        <div className="container mx-auto flex md:flex-row flex-col justify-between gap-10">
-          <div className="w-full h-full rounded-t-full rounded-l-full drop-shadow-lg overflow-hidden ">
-            <Image src="/Fabricio.jpg" alt="Especialista" width={700} height={700} className="w-auto h-auto object-contain  hover:scale-105 duration-300 transition-all" />
-          </div>
-          <div className="flex items-center flex-col w-full p-10">
-            <h2 className="text-3xl font-semibold text-center">
-              Fabrício Maciel, MD, MSc
-            </h2>
-            <p className="text-center text-gray-500 mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, ligula auctor aliquet.</p>
-          </div>
-        </div>
-
-      </section>
+      <AboutSection />
+      <ServicesSection />
     </>
   );
 }
