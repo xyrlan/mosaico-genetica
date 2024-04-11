901de2ed29569a1306e0ca820c148b05f358ffb1
xyrlan <lobatopedro68@gmail.com>
2024-04-11 13:11:16 -0400

    Integrate @splinetool/react-spline into SplineViewer component and update layout in page.tsx

--- src/app/components/SplineViewer.tsx
+++ src/app/components/SplineViewer.tsx
@@ -3,11 +3,13 @@ import Script from 'next/script';
 import Spline from '@splinetool/react-spline';
 
 const SplineViewer = () => {
+const [isHidden, setIsHidden] = React.useState(true)
+
   return (
     <div className='hidden lg:block'>
       <Script type="module" src="https://unpkg.com/@splinetool/viewer@1.0.93/build/spline-viewer.js" strategy="beforeInteractive" ></Script>
       <div className="h-full w-full md:w-1/2 flex-none z-10 max-md:opacity-20 right-0 top-0 absolute">
-        <Spline scene="https://prod.spline.design/ZNUUDkDh9yJcH65u/scene.splinecode"
+        <Spline className={isHidden ? 'hidden' : 'block'} onLoad={() => setIsHidden(false)} scene="https://prod.spline.design/ZNUUDkDh9yJcH65u/scene.splinecode"
       />
         {/* <div className="absolute bottom-4 right-2 bg-gray-200 h-10 w-40 z-20  " /> */}
       </div>
